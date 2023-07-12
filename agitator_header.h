a981de1fb9fc7c07d993e166023af8da4828988e
xXxMKAYxXx <134626719+xXxMKAYxXx@users.noreply.github.com>
2023-07-12 13:58:07 +0100

    Update agitator_header.h
    Added serial prints for debugging
    Can correctly resume a cycle (with correct duration( after one pause only)
    If user hits pause again, the previous pausedTime no longer be taken into account -> will functions as if the cycle was running at that time.

--- agitator_header.h
+++ agitator_header.h
@@ -2,33 +2,31 @@
 int full_cycle();
 int yoyo_cycle();
 int sway_rotate();
-int stop_cycle();
+int pause_cycle();
+int exit_cycle();
 int print_cycle_done();
 int print_options();
+int print_cycle_done();
 
 //--------------- Define -----------------------------------------//
 #define CYCLE_DURATION 10000
 
 //---------------- Objects ---------------------------------------//
 
-Servo LittleServo;
 Servo BigServo;
-
+Servo LittleServo;
 
 //--------------- declaring global variables ----------------------//
-int inputCycle = 0;
-bool stopCycle = false;
+int   inputCycle = 0;
+bool  pausedCycle = false;
 
-int rotationTime = 0;  
-int d = 0;              //period between different command signals
-int n = 0;              //number of toggles
-int vitesseSens1 = 0;
-int vitesseSens2 = 0;
 
 //-------- Timers
-unsigned long startCycleMillis;
-unsigned long fullCycleMillis;
-unsigned long cycleRunningTime;
+unsigned long startCycleMillis = 0;
+unsigned long currentTimeMillis = 0;
+unsigned long cycleRunningTime = 0;
+unsigned long cyclePausedTime = 0;
+unsigned long pauseCycleMillis = 0; // intialisition to this variable is necessary
 
 
 int get_cycle(){
@@ -36,20 +34,23 @@ int get_cycle(){
    if (Serial.available() > 0) {
     int userInput = Serial.read();
     switch (userInput) {
-      case 's':
-      stopCycle = true;
-      Serial.print("Cycle Stopped\n");
+      case 'x' :
+      exit_cycle();
+      break;
+
+      case 'p':
+      pause_cycle();
       break;
       
       case 'c':
-      stopCycle = false;
+      pausedCycle = false;
       Serial.print("Cycle Resumed\n");
       break;
       
       case '1' :
       case '2':
       case '3':
-      stopCycle = false;
+      pausedCycle = false;
       inputCycle = userInput - 48;
       startCycleMillis = millis();
       break;
@@ -64,27 +65,27 @@ int full_cycle(){
   
    if(inputCycle == 1){
      //-- Get current time 
-     fullCycleMillis = millis();
+     currentTimeMillis = millis(); //lacks clarity in naming && no definition
      
      //-- Calculate time period since the cycle started 
-     cycleRunningTime = fullCycleMillis - startCycleMillis; 
+     cycleRunningTime = currentTimeMillis - startCycleMillis - cyclePausedTime; 
+
+     Serial.print("cycleRunningTime = "); Serial.print(cycleRunningTime);Serial.print("\n");
+     Serial.print("cyclePausedtime\t");Serial.print(cyclePausedTime);Serial.print("\n");
      
      if (cycleRunningTime < CYCLE_DURATION){
         
         LittleServo.write(150);
         //-------- if the number of seconds is pair -> run clockwise
-        if ((trunc((cycleRunningTime/1000) % 2)) == 0){
-          BigServo.write(0); 
+        /*if ((trunc((cycleRunningTime/1000) % 2)) == 0){
+          BigServo.write(50); 
         }
         else{ //run counter clockwise
-          BigServo.write(180); 
-        } 
+          BigServo.write(150); 
+        } */
      }
      else{
-      stop_cycle();
-      print_cycle_done();
-      print_options();
-      inputCycle = 0; // so it doesnt enter this branch event if 
+      exit_cycle();
      }
    }
 }
@@ -97,9 +98,33 @@ int sway_rotate(){
    ;  
 }
 
-int stop_cycle(){
+int exit_cycle(){
   LittleServo.write(90);
-  BigServo.write(0);
+  print_cycle_done();
+  print_options(); 
+  inputCycle = 0; 
+}
+
+int pause_cycle(){
+    LittleServo.write(90);
+    Serial.print("Cycle Paused\n"); 
+    pauseCycleMillis = millis(); // time when the user last pressed pause
+    pausedCycle = true; // ? ambiguous variable, hmmmmm ?
+}
+
+int idle_state(){
+  LittleServo.write(90);
+
+  if((millis() % 500 ) == 0)
+  Serial.print("idle\n");
+
+  if(inputCycle){ // if there s an ongoing cycle
+    cyclePausedTime = millis() - pauseCycleMillis; //time passed since last time pause was pressed
+
+    if((millis() % 500 ) == 0){
+      Serial.print("cyclePausedtime\t");Serial.print(cyclePausedTime);Serial.print("\n");
+    }
+  }
 }
 
 int print_options(){
@@ -113,3 +138,4 @@ int print_options(){
 int print_cycle_done(){
   Serial.print("Cycle Done!\n");
 }
+
